58d0df75421ab0bbf58a0e168fb66d564d2ba978
Shrikant Singh <Shrikant@cloudthing.com>
2019-12-19 15:07:41 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "%7Bshri_kant%7D.github.io/todo-list/precache-manifest.bb59e50c5921458ca6db3dfd7847a4eb.js"
+  "/todo-list/precache-manifest.5f1204b0e4b26ba68740c45f27872ae3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("%7Bshri_kant%7D.github.io/todo-list/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
